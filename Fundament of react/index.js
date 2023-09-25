531b7a7b86944e830da9e89cf1cc4876dd485010
Nightmare St <kisonamplay@gmail.com>
2023-09-25 20:45:19 +0200

    work with node.js

--- Fundament of react/index.js
+++ Fundament of react/index.js
@@ -1,1 +1,27 @@
-ReactDOM.render(<p>Hello World from React</p>, document.getElementById("root"));
+// ReactDOM.render(
+//     <div>
+//         <h1>Hello World!</h1>
+//         <p>Second text</p>
+//     </div>,
+//     document.getElementById("root")
+// ); 
+
+// First variant --
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//      <div>
+//         <h1>Hello World!</h1>
+//         <p>Second text</p>
+//     </div>
+// );   
+
+// --
+
+// second variant --
+ReactDOM.createRoot(document.getElementById("root")).render(
+     /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/
+     React.createElement("h1", null, "Hello World!"), /*#__PURE__*/
+     React.createElement("p", null, "Second text"))
+);
+// --
